6c1c3f556843364bb8219d18cf811c24d4490142
ivorita <630150119@qq.com>
2018-11-18 18:27:09 +0800

    Update APIService.java

--- app/src/main/java/com/antelope/android/bottomnavigation/net/APIService.java
+++ app/src/main/java/com/antelope/android/bottomnavigation/net/APIService.java
@@ -34,32 +34,32 @@ public class APIService {
     }
 
     public interface APIs{
-        @Headers("api-key:Uv=e=yMBymo8In9FVA4Ub16Oleo=")
-        @GET("devices/30964714/datastreams/pic10")
+        @Headers("api-key:xxx")
+        @GET("devices/xxxxxxxx/datastreams/pic10")
         Flowable<DS<Picture>> latestPic();
 
-       /*@Headers("api-key:Uv=e=yMBymo8In9FVA4Ub16Oleo=")
+       /*@Headers("api-key:xxx")
         @GET("bindata/{index}")
         Flowable<Picture> getPic(@Path("index") String index);*/
 
-        @Headers("api-key:Uv=e=yMBymo8In9FVA4Ub16Oleo=")
-        @GET("devices/30964714/datastreams/Temperature")
+        @Headers("api-key:xxx")
+        @GET("devices/xxxxxxxx/datastreams/Temperature")
         Flowable<DS<IntegerData>> lastestTemp();
 
-        @Headers("api-key:Uv=e=yMBymo8In9FVA4Ub16Oleo=")
-        @GET("devices/30964714/datastreams/Humidity")
+        @Headers("api-key:xxx")
+        @GET("devices/xxxxxxxx/datastreams/Humidity")
         Flowable<DS<IntegerData>> lastestHum();
 
-        @Headers("api-key:Uv=e=yMBymo8In9FVA4Ub16Oleo=")
-        @GET("devices/30964714/datastreams/door:")
+        @Headers("api-key:xxx")
+        @GET("devices/xxxxxxxx/datastreams/door:")
         Flowable<DS<IntegerData>> lastestDState();
 
-        @Headers("api-key:Uv=e=yMBymo8In9FVA4Ub16Oleo=")
-        @GET("devices/30964714/datastreams/{data}")
+        @Headers("api-key:xxx")
+        @GET("devices/xxxxxxxx/datastreams/{data}")
         Flowable<DS<IntegerData>> latest(@Path("data") String data);
 
-        /*@Headers("api-key:Uv=e=yMBymo8In9FVA4Ub16Oleo=")
-        @POST("cmds?device_id=30964714&qos=0&timeout=300")*/
+        /*@Headers("api-key:xxx")
+        @POST("cmds?device_id=xxxxxxxx&qos=0&timeout=300")*/
 
     }
 
